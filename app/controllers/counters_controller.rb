ab47db03b79836bb41cb7ca29495e78e9c41e77b
Troy <troyakins99@yahoo.com>
2021-10-18 23:44:02 -0500

    created count function, excluded 3 unnecessary boiler plate json data objects

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -1,2 +1,7 @@
 class CountersController < ApplicationController
+
+    def index 
+        count = Counter.all
+    render json: count.to_json(except: [:created_at, :updated_at, :id])
+    end
 end
