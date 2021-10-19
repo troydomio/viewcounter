4fb88bd1e0b3fa5a1fb0a350afe98361104243ce
Troy <troyakins99@yahoo.com>
2021-10-18 23:57:13 -0500

    incrementer working on page refresh

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -1,7 +1,8 @@
 class CountersController < ApplicationController
 
     def index 
-        count = Counter.all
+        count = Counter.find(1)
+        Counter.increment_counter(:view, 1)
     render json: count.to_json(except: [:created_at, :updated_at, :id])
     end
 end
